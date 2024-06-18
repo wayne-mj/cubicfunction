3cccae04bf9ac32f07695632841cf9df7c7a7174
Wayne.MJ <150761502+wayne-mj@users.noreply.github.com>
2024-06-19 00:44:56 +1000

    A rewrite of the code

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 FC=gfortran
 FFLAGS=-O3 -Wall -Wextra
-MODULES=factors.f90 mergesort.f90 quadratic.f90 newton.f90
+MODULES=factors.f90 mergesort.f90 quadratic.f90 newton.f90 rationalroot.f90
 PROG=cubic.f90
 SRC=$(MODULES) $(PROG)
 OBJ=${SRC:.f90=.o}
--- cubic.f90
+++ cubic.f90
@@ -3,26 +3,22 @@ program cubic
     use mergesort
     use quadratic
     use newton
+    use rationalroot
     implicit none
 
     ! Rewriten to work with real numbers
-    integer :: a, b, c, d, t, i,j, arrsize, count
-    integer, allocatable :: q(:), p(:)
+    integer :: a, b, c, d, i
     integer :: x, xp 
     integer, parameter :: start = -1
     integer, parameter :: end = 5
-    real :: x1, x2, x3, rem, y, cx1, cx2, cxi, qa, qb, qc, xreal, lastx1
-    real, allocatable :: pq(:), temp(:)
+    real :: y, xreal, xout 
+    real, allocatable, dimension(:) :: pq
+    real, allocatable, dimension(:) :: temp
     character(1), parameter :: newline = achar(10)
-    logical :: graph, notone, notminusone, found
+    logical :: graph
 
     ! Set the flags to false
-    notone = .false.
-    notminusone = .false.
     graph = .false.
-    found = .false.
-
-    count =1
 
     ! A little bit of a menu
     print *, newline // 'Cubic equation solver' // newline // 'Enter the coefficients of the equation ax^3 + bx^2 + cx + d = 0' // newline
@@ -30,99 +26,21 @@ program cubic
     ! Get the coefficients all at once
     write (*, '(A)', advance='no') 'Enter a, b, c, d: '
     read (*,*) a, b, c, d
+
     
-    ! print *, a,b,c,d
-    ! Check if x = 1
-    t = a + b + c + d
-    if (t .eq. 0) then
-        notone = .true.
-        x1 = 1
-    end if
+    pq = candidates(a, d)
 
-    ! Check if x = -1
-    if ((a+c) .eq. (b+d)) then
-        notminusone = .true.
-        x1 = -1
-    end if
-
-    ! Factor p/q where p = d, and q = a
-    q = factoring(a)
-    p = factoring(d)
-
-    ! Calculate p / q
-    arrsize = (size(p)*size(q)) * 2
-    allocate(pq(arrsize))
-    do i = 1, size(q)
-        do j = 1, size(p)
-            pq(count) = real(p(j)) / real(q(i))
-            count = count + 1
-        end do
+    do i = 1, size(pq)
+        temp = syntheticdivision(a, b, c, d, pq(i))
+        print *, temp
     end do
-
-    do i = 1, size(q)
-        do j = 1, size(p)
-            pq(count) = -(real(p(j)) / real(q(i)))
-            count = count + 1
-        end do
-    end do
-
-    call mergesort_real(pq, size(pq))
     
-    ! Debugging
-    !print *, "Factors: ", pq
+    print *, " "
 
-    ! Synthetically divide the factors
-    ! If the remainder is zero, then the factor is a root
-    if (notone .eqv. .false. .or. notminusone .eqv. .false.) then
-        qa = a
-        qb = (qa * x1) + b
-        qc = (qb * x1) + c
-        rem = (qc * x1) + d
-        call quad_roots(qa, qb, qc, x2, x3)
-        call criticalpoints(a, b, c, cx1, cx2, cxi)
-        print *, "Roots: ", x1, x2, x3
-        print *, "Critical points: ", cx1, cx2, cxi
-        graph = .true.
-        found = .true.
-    else
-        do i = 1, size(pq)
-            qa = a
-            qb = (qa * pq(i)) + b
-            qc = (qb * pq(i)) + c
-            rem = (qc * pq(i)) + d
-
-            ! Debugging
-            ! print *, "Remainder: ", rem
-
-            if (rem == 0.) then
-                call quad_roots(qa, qb, qc, x2, x3)
-                call criticalpoints(a, b, c, cx1, cx2, cxi)
-                print *, "Roots: ", pq(i), x2, x3
-                print *, "Critical points: ", cx1, cx2, cxi
-                graph = .true.
-                found = .true.
-             else
-                 print *, "No rational roots found."
-                 found = .false.
-                !  print *, "Trying Newton Raphson method."
-                !  call newton_r(a, b, c, d, pq(i), 1.0e-7, x1)
-                !  print *, "Possible root: ", x1
-            end if
-        end do
-
-        ! If no rational roots are found, try the Newton Raphson method
-        if (found .eqv. .false.) then
-            print *, "Trying Newton Raphson method."
-            do i = 1, 150
-                call newton_r(a, b, c, d, real(i), 1.0e-7, x1)
-                ! Remove repeated roots
-                if (x1 .ne. lastx1) then
-                    print *, "Possible root: ", x1
-                end if
-                lastx1 = x1
-            end do
-        end if
-    end if
+    do i = 1, size(pq)
+        call newton_r(a, b, c, d, pq(i), 1e-7, xout)
+        print *, xout
+    end do
 
     ! If there are real roots, write the data to a file
     if (graph .eqv. .true.) then
@@ -137,4 +55,5 @@ program cubic
         flush (10)
         close (10)
     end if
+
 end program cubic
